524f135df4ed1f451288ca268dca750768a053ce
ngdevelop-tech <aprajapati06@gmail.com>
2021-04-10 13:46:58 +0530

    Scully | Using Copy To Clipboard Plugin

--- scully.portfolio.config.ts
+++ scully.portfolio.config.ts
@@ -1,6 +1,14 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { copyToClipboard, CopyToClipboardPluginConfig } from '@scullyio/scully-plugin-copy-to-clipboard';
 
 setPluginConfig('md', { enableSyntaxHighlighting : true});
+
+setPluginConfig<CopyToClipboardPluginConfig>(copyToClipboard, {
+  copyBtnInitialText : 'Copy 📄',
+  copyBtnOnClickText : 'Copied! ✅',
+  customBtnClass: 'copyToClipboardBtn',
+  clipboardJSPath : 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js',
+})
 
 export const config: ScullyConfig = {
   projectRoot: "./src",
@@ -9,6 +17,7 @@ export const config: ScullyConfig = {
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
+      postRenderers: [ copyToClipboard ],
       slug: {
         folder: "./blog"
       }
